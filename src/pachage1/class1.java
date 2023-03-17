689a1a1b80d089723017e30abf7d1eed57bca357
test <testemail@test.com>
2023-03-17 16:49:23 +0100

    first commit

---
+++ src/pachage1/class1.java
@@ -0,0 +1,7 @@
+package pachage1;
+
+public class class1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to java learning");
+    }
+}
---
+++ src/pachage1/class2.java
@@ -0,0 +1,7 @@
+package pachage1;
+
+public class class2 {
+    public static void main(String[] args) {
+        System.out.println("Life is beautiful");
+    }
+}
